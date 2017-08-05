87be414f0d3ea8b1599caf3fdb890ef002efcd33
Alex Ao <aozhimin0811@gmail.com>
2017-08-05 22:57:20 +0800

    🚀 Add project

---
+++ AAFingerprint/AAFingerprint/AAFingerprint.h
@@ -0,0 +1,19 @@
+//
+//  AAFingerprint.h
+//  AAFingerprint
+//
+//  Created by dev-aozhimin on 2017/8/5.
+//  Copyright © 2017年 aozhimin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AAFingerprint.
+FOUNDATION_EXPORT double AAFingerprintVersionNumber;
+
+//! Project version string for AAFingerprint.
+FOUNDATION_EXPORT const unsigned char AAFingerprintVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AAFingerprint/PublicHeader.h>
+
+
